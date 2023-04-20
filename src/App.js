397a5ef40cbe7d45df48d815c80fcac2d7cbe7f5
DIGIPlus <admin@digiplus.cf>
2023-04-20 20:10:07 +0400

    Box dynamic style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react"
 import boxes from "./boxes"
 import "./App.css"
+import Box from "./Box"
 
 export default function App(props) {
     const [squares, setSquares] = React.useState(boxes)
     
-    const styles = {
-        backgroundColor: props.darkMode ? "#222222" : "#cccccc"
-    }
+    // const styles = {
+    //     backgroundColor: props.darkMode ? "#222222" : "#cccccc"
+    // }
     
     const squareElements = squares.map(square => (
-        <div style={styles} className="box" key={square.id}></div>
+        <Box key={square.id} on={square.on}/>
     ))
     return (
         <main>
---
+++ src/Box.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Box(props){
+    const styles = {
+        backgroundColor: props.on ? "#222222" : "none"
+    }
+    return (
+        <div style={styles} className="box"></div>
+    )
+}
