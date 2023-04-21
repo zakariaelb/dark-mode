82fd49ea84d00c24d2537b278cfcee2b32e19623
DIGIPlus <admin@digiplus.cf>
2023-04-21 19:16:26 +0400

    Master update

--- src/Box.js
+++ src/Box.js
@@ -1,9 +1,13 @@
 import React from "react";
 export default function Box(props){
+    const [on, setOn] = React.useState(props.on)
     const styles = {
-        backgroundColor: props.on ? "#222222" : "none"
+        backgroundColor: props.on ? "#222222" : "transparent"
+    }
+    function toggole() {
+        setOn(prevOn + !prevOn)
     }
     return (
-        <div style={styles} className="box"></div>
+        <div style={styles} className="box" onClick={toggole}></div>
     )
 }
