8a49360d7b733b38e9b9ef3d2d59a88c0697dbb1
DIGIPlus <admin@digiplus.cf>
2023-04-24 20:43:03 +0400

    toggle

--- src/App.js
+++ src/App.js
@@ -10,24 +10,36 @@ export default function App() {
     //     backgroundColor: props.darkMode ? "#222222" : "#cccccc"
     // }
 
-    function toggle(id){
-         setSquares(prevSauares => {
-             const newSquares = []
-             for(let i = 0; i < prevSauares.length; i++) {
-                 const currentSquares = prevSauares[i]
-                 if(currentSquares.id === id){
-                     const updatedSquares = {
-                         ...currentSquares, 
-                         on: !currentSquares.on
-                     }
-                     newSquares.push(updatedSquares)
-                 } else {
-                     newSquares.push(currentSquares)
-                 }
-             }
-             return newSquares
-         })
-     }
+    // function toggle(id){
+    //      setSquares(prevSauares => {
+            //  const newSquares = []
+            //  for(let i = 0; i < prevSauares.length; i++) {
+            //      const currentSquares = prevSauares[i]
+            //      if(currentSquares.id === id){
+            //          const updatedSquares = {
+            //              ...currentSquares, 
+            //              on: !currentSquares.on
+            //          }
+            //          newSquares.push(updatedSquares)
+            //      } else {
+            //          newSquares.push(currentSquares)
+            //      }
+            //  }
+            //  return newSquares
+    //      })
+    //  }
+
+     //Do this instead //
+
+     function toggle(id){
+        setSquares(prevSauares => {
+            return prevSauares.map((square) => {
+               return square.id === id ? {...square, on: !square.on} : square
+
+            })
+
+        })
+    }
 
     const squareElements = squares.map(square => (
         <Box 
